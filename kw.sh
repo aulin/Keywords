575bc41c812b473b88edbe2e91381321a959b603
Johan Aulin <johan.aulin@gmail.com>
2012-04-17 19:29:14 +0200

    Add commends and remove fancy colors and headers

--- kw.sh
+++ kw.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Listar de av mina anteckningsfiler som innehåller
+# angivet nyckelord i nyckelordsraden.
+
 for p in "$@";
 do
-    echo "";
-    echo "### $p ###";
-    grep -lr --color=auto ^Keywords:.*"$p".*$ /home/johan/notes/*.txt;
+	# Med fin rubrik och i färg:
+
+    #echo "";
+    #echo "### $p ###";
+    #grep -lr --color=auto ^Keywords:.*"$p".*$ /home/johan/notes/*.txt;
+
+	# Utan krimskrams:
+	grep -lr ^Keywords:.*"$p".*$ /home/johan/notes/*.txt;
 done;
