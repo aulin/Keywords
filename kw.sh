fd6f219ab620204bf95c624269b2b2810ed6cb96
Johan Aulin <johan.aulin@gmail.com>
2012-04-18 22:14:50 +0200

    Enable piping found files to other commands
    Also add usage message for empty arguments

--- kw.sh
+++ kw.sh
@@ -7,28 +7,57 @@ directory="/home/johan/notes/"
 extension="*.txt"
 files="${directory}${extension}"
 
-fancy=0
+pipeable=0
+opts="-lr"
+is_cmd=0
+cmd=""
+
+if [ -z $1 ]
+then
+	echo "Usage: kw [-p command] [keyword list...]"
+	echo ""
+	echo "Lists files in the directory specified"
+	echo "in the script, with the specified file"
+	echo "extension that contain rows prefixed"
+	echo 'with "Keywords:" or "Title:" that contain'
+	echo "the keywords given on the command line."
+	echo ""
+	echo "Options:"
+	echo "-p command    Pipe the output to the"
+	echo "              specified command via"
+	echo "              null terminals and xargs."
+	echo ""
+	echo "Example: kw recipe"
+	echo "Example: kw -p vim recipe"
+
+	exit 0
+fi
 
 for q in "$@"
 do
-	if [ $q = "-f" ]
+	if [ $is_cmd = 1 ]
 	then
-		if [ $fancy -eq 0 ]
+		# It's a command. Use it to build a pipe
+		cmd=$q
+		is_cmd=0
+	elif [ $q = "-p" ] || [ $q = "--pipeable" ]
+	then
+		# It's an option. Use null terminals instead
+		# of newlines and prepare for a command
+		opts=${opts}" -Z"
+		pipeable=1
+		is_cmd=1
+	else
+		# Not an option, so treat as keyword
+		if [ $pipeable = 0 ]
 		then
 			# Use fancy headings and colors
-			fancy=1
+			opts=$opts" --color=auto"
+			echo ""
+			echo "### $q ###"
+			grep $opts -e ^Keywords:.*"$q".*$ -e ^Title:.*"$q".*$ $files
 		else
-			# No bells and whistles
-			fancy=0
-		fi
-	else
-		if [ $fancy -eq 1 ]
-		then
-		    echo "";
-			echo "### $q ###";
-			grep -lr --color=auto -e ^Keywords:.*"$q".*$ -e ^Title:.*"$q".*$ $files;
-		else
-			grep -lr -e ^Keywords:.*"$q".*$ -e ^Title:.*"$q".*$ $files;
+			grep $opts -e ^Keywords:.*"$q".*$ -e ^Title:.*"$q".*$ $files | xargs -0 $cmd
 		fi
 	fi
 done
