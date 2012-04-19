c97bd759b3562d250701048c7db9ed295fffa606
Johan Aulin <johan.aulin@gmail.com>
2012-04-19 17:40:23 +0200

    Update comments and simplify a variable name

--- kw.sh
+++ kw.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-# List those notes that contain the given keyword
-# among their keywords or in their title.
+# Given files which contain rows that begin
+# with "Title:" and "Keywords:", list those
+# files that contain the given keyword[s]
+# in either of those rows.
 
 directory="/home/johan/notes/"
 extension="*.txt"
 files="${directory}${extension}"
 
-pipeable=0
+pipe=0
 opts="-lr"
 is_cmd=0
 cmd=""
 
 if [ -z $1 ]
 then
-	echo "Usage: kw [-p command] [keyword list...]"
-	echo ""
-	echo "Lists files in the directory specified"
-	echo "in the script, with the specified file"
-	echo "extension that contain rows prefixed"
-	echo 'with "Keywords:" or "Title:" that contain'
-	echo "the keywords given on the command line."
-	echo ""
-	echo "Options:"
-	echo "-p command    Pipe the output to the"
-	echo "              specified command via"
-	echo "              null terminals and xargs."
-	echo ""
-	echo "Example: kw recipe"
-	echo "Example: kw -p vim recipe"
+	echo 'Keywords by Johan Aulin'
+	echo ''
+	echo 'Usage: kw [-p command] [keyword list...]'
+	echo ''
+	echo 'Given files which contain rows that begin'
+	echo 'with "Title:" and "Keywords:", list those'
+	echo 'files that contain the given keyword[s]'
+	echo 'in either of those rows.'
+	echo ''
+	echo 'Options:'
+	echo '-p command    Pipe the output to the'
+	echo '              specified command via'
+	echo '              null terminals and xargs.'
+	echo ''
+	echo 'Example: kw recipe'
+	echo 'Example: kw -p vim recipe'
 
 	exit 0
 fi
@@ -40,16 +43,16 @@ do
 		# It's a command. Use it to build a pipe
 		cmd=$q
 		is_cmd=0
-	elif [ $q = "-p" ] || [ $q = "--pipeable" ]
+	elif [ $q = "-p" ] || [ $q = "--pipe" ]
 	then
 		# It's an option. Use null terminals instead
 		# of newlines and prepare for a command
 		opts=${opts}" -Z"
-		pipeable=1
+		pipe=1
 		is_cmd=1
 	else
 		# Not an option, so treat as keyword
-		if [ $pipeable = 0 ]
+		if [ $pipe = 0 ]
 		then
 			# Use fancy headings and colors
 			opts=$opts" --color=auto"
